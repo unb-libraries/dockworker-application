c824bb1e37acd9d8579f64595a67f1b8062e3ed1
Jacob Sanford <jsanford@unb.ca>
2023-05-04 18:22:22 -0300

    Update GitHubRepoSettingsTrait

--- src/Robo/Plugin/Commands/GitHubRepositorySettingsCommands.php
+++ src/Robo/Plugin/Commands/GitHubRepositorySettingsCommands.php
@@ -7,6 +7,7 @@ use Consolidation\AnnotatedCommand\Events\CustomEventAwareTrait;
 use Dockworker\DockworkerApplicationCommands;
 use Dockworker\GitHub\GitHubRepoSettingsTrait;
 use Dockworker\IO\DockworkerIOTrait;
+use Robo\Robo;
 use Symfony\Component\Yaml\Yaml;
 
 /**
@@ -21,20 +22,55 @@ class GitHubRepositorySettingsCommands extends DockworkerApplicationCommands imp
     /**
      * Sets topics for the repository.
      *
-     * @command github:repository:set-topics
+     * @command github:repository:set-metadata
      * @hidden
      */
-    public function setGitHubTopics(): void
+    public function setRepositoryMetadata(): void
     {
-        $this->initGitHubRepoSettingsCommands();
-        $this->dockworkerIO->title("Setting GitHub Repository Topics");
-        $topics = $this->getGitHubRepositoryTopics();
+        $this->initRepositorySettings();
+        $this->setRepositoryDescription();
+        $this->setRepositoryTopics();
+    }
+
+    protected function setRepositoryDescription(): void
+    {
+        $this->dockworkerIO->title("Setting GitHub Repository Description");
         try{
-            $this->dockworkerIO->listing($topics);
-            $currentTopics = $this->gitHubClient->api('repo')->replaceTopics(
+            $description = Robo::Config()->get('dockworker.application.description');
+            $uri = Robo::Config()->get('dockworker.application.uri');
+            $this->dockworkerIO->block($description);
+            $this->dockworkerIO->block($uri);
+             $this->writeGitHubRepositoryDescription(
                 $this->applicationGitHubRepoOwner,
                 $this->applicationGitHubRepoName,
-                $topics
+                 [
+                     'description' => $description,
+                     'homepage' => $uri,
+                     'has_wiki' => false,
+                     'has_issues' => false,
+                     'has_downloads' => false,
+                 ]
+            );
+            $this->say("GitHub repository description set successfully!");
+        }
+        catch (\Exception $e) {
+            $this->dockworkerIO->error("Unable to set GitHub repository description: " . $e->getMessage());
+            exit(1);
+        }
+    }
+
+    protected function setRepositoryTopics(): void
+    {
+        $this->dockworkerIO->title("Setting GitHub Repository Topics");
+        $this->clearGitHubRepositoryTopics();
+        $this->addGitHubRepositoryTopics(
+            $this->getGitHubRepositoryTopics()
+        );
+        try{
+            $this->dockworkerIO->listing($this->gitHubRepositoryTopics);
+            $this->writeGitHubRepositoryTopics(
+                $this->applicationGitHubRepoOwner,
+                $this->applicationGitHubRepoName
             );
             $this->say("GitHub repository topics set successfully!");
         }
@@ -44,7 +80,7 @@ class GitHubRepositorySettingsCommands extends DockworkerApplicationCommands imp
         }
     }
 
-    protected function initGitHubRepoSettingsCommands(): void
+    protected function initRepositorySettings(): void
     {
         $this->initGitHubClientApplicationRepo(
             $this->applicationGitHubRepoOwner,
@@ -58,6 +94,11 @@ class GitHubRepositorySettingsCommands extends DockworkerApplicationCommands imp
         $file_path = "$this->applicationRoot/vendor/unb-libraries/dockworker-application/data/github.yml";
         $data = Yaml::parseFile($file_path);
         $topics = $data['github']['repository']['topics'];
+
+        foreach (Robo::Config()->get('dockworker.application.topics') as $topic) {
+            $topics[] = $topic;
+        }
+
         $handlers = $this->getCustomEventHandlers('dockworker-github-topics');
         foreach ($handlers as $handler) {
             $new_topics = $handler();
@@ -68,4 +109,5 @@ class GitHubRepositorySettingsCommands extends DockworkerApplicationCommands imp
         }
         return $topics;
     }
+
 }
